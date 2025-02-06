8550f4350bb67e9c1a17a08e3a1e540f3856e04f
William Pompei <let.s.ai.fun@gmail.com>
2025-02-06 11:15:22 +0100

    add const for LLM and Embeddigns model
    added constants to reference llm and embeddings model name for easier update

--- my_types.py
+++ my_types.py
@@ -6,3 +6,11 @@ class CHAIN_TYPE(Enum):
     """
     CHAIN_SIMPLE = 1
     CHAIN_RAG = 2
+
+# ollama LLM model used: update here if you want to use a different LLM from Ollama
+OLLAMA_MODEL_NAME= "llama3.2"
+
+# ollama EMBEDDINGS model used: update here if you want to use a different EMBEDDINGS from Ollama
+OLLAMA_EMBEDDINGS_NAME= "nomic-embed-text"
+
+RAG_URLS = []
